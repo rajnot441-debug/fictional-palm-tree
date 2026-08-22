5a24e23f740e417c9f4f714876de4076f7d1ef07
rajnot441-debug <rajnot441@gmail.com>
2026-08-22 20:05:37 +0530

    Add AppColors class for color constants

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textSecondary = Colors.grey;
+  static const Color bull = Colors.green;
+  static const Color bear = Colors.red;
+}
